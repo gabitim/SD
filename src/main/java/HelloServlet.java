ee2bb4d641f6989cd81e0bdd59348568b85be666
Timofti Gabriel <gabitim470@gmail.com>
2021-03-04 01:29:47 +0200

    basic project done

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Timofti Gabriel
+ */
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse)
+            throws ServletException, IOException {
+        httpServletResponse.getWriter().print("Hello from servlet");
+    }
+}
